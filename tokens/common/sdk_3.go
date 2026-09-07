d9ed16bc6dde760f7deb765118016d4e175cd5a9
Atharva Borade <atharvaborade568@gmail.com>
2026-09-07 17:44:22 +0530

    Bump fabric-token-sdk and fabric-smart-client to v0.12.1 (#27)
    Signed-off-by: atharrva01 <atharvaborade568@gmail.com>

--- tokens/common/sdk_3.go
+++ tokens/common/sdk_3.go
@@ -9,31 +9,19 @@ SPDX-License-Identifier: Apache-2.0
 package common
 
 import (
-	"errors"
-
+	common "github.com/hyperledger-labs/fabric-smart-client/platform/common/sdk/dig"
 	"github.com/hyperledger-labs/fabric-smart-client/platform/view/services"
-	dlog "github.com/hyperledger-labs/fabric-token-sdk/token/core/zkatdlog/nogh/v1/driver"
-	tokensdk "github.com/hyperledger-labs/fabric-token-sdk/token/sdk/dig"
-	"github.com/hyperledger-labs/fabric-token-sdk/token/services/network/fabric"
-	"go.uber.org/dig"
+	"github.com/hyperledger-labs/fabric-token-sdk/integration/token/common/sdk/fdlog"
 )
 
 func NewSDK(registry services.Registry) *SDK {
-	return &SDK{
-		tokensdk.NewSDK(registry),
-	}
+	return &SDK{SDK: fdlog.NewSDK(registry)}
 }
 
 type SDK struct {
-	*tokensdk.SDK
+	common.SDK
 }
 
 func (p *SDK) Install() error {
-	if err := errors.Join(
-		p.Container().Provide(fabric.NewGenericDriver, dig.Group("network-drivers")),
-		p.Container().Provide(dlog.NewDriver, dig.Group("token-drivers")),
-	); err != nil {
-		return err
-	}
 	return p.SDK.Install()
 }
